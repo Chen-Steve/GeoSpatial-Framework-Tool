ba080312b5c34606c3885042773c3ae1123605ab
Steven <zhangc2@illinois.edu>
2023-10-06 00:32:28 -0500

    attempt search query functions

--- dashboard.py
+++ dashboard.py
@@ -1,30 +1,38 @@
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
-import plotly.express as px
 import geopandas as gpd
+import folium
+from folium.plugins import Search
+from data_processing import load_shapefiles
 
-# Load data
-gdf = gpd.read_file("path_to_your_data.shp")
+# Load the data
+all_data = load_shapefiles("data")
 
-app = dash.Dash(__name__)
+# Initialize the map
+m = folium.Map(location=[45.75, 126.65], zoom_start=10)  # Coordinates for Harbin
 
-app.layout = html.Div([
-    dcc.Input(id='search-box', type='text', placeholder='Enter feature...'),
-    dcc.Graph(id='map-output')
-])
+# List of features to add to the map
+features = [
+    {"name": "airport", "geom_type": "Point", "color": "blue"},
+    {"name": "subway_entrance", "geom_type": "Point", "color": "red"},
+    {"name": "buildings", "geom_type": "Polygon", "color": "green"},
+    # Add more features as needed
+]
 
-@app.callback(
-    Output('map-output', 'figure'),
-    [Input('search-box', 'value')]
-)
-def update_map(search_value):
-    if search_value == "airport":
-        filtered_gdf = gdf[gdf['type'] == 'airport']
-        fig = px.choropleth_mapbox(filtered_gdf, ... )  # Create map using plotly express
-        return fig
-    # Add conditions for other features
-    return dash.no_update
+for feature in features:
+    feature_key = f"{feature['name']}-polygon" if feature['geom_type'] == "Polygon" else f"{feature['name']}-point"
+    if feature_key in all_data:
+        geojson_data = all_data[feature_key].to_json()
+        folium.GeoJson(geojson_data, name=feature['name'], style_function=lambda x: {'fillColor': feature['color']}).add_to(m)
+        
+        # Add search functionality for each feature
+        search = Search(
+            layer=folium.GeoJson(geojson_data),
+            geom_type=feature['geom_type'],
+            placeholder=f"Search for {feature['name']}",
+            collapsed=False,
+        ).add_to(m)
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# Add layer control to toggle on/off
+folium.LayerControl().add_to(m)
+
+# Save the map to an HTML file
+m.save("map.html")
