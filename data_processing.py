383f90f6bf4731c4b5b265fc2a937743df697c00
Steven <zhangc2@illinois.edu>
2023-10-08 19:13:38 -0500

    added overlay options

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,5 @@
 import geopandas as gpd
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-from matplotlib.widgets import Button
-from svgpath2mpl import parse_path
+import folium
 import os
 
 def load_shapefiles(directory):
@@ -14,25 +11,7 @@ def load_shapefiles(directory):
             shapefiles[layer_name] = gpd.read_file(filepath)
     return shapefiles
 
-def plot_svg(ax, svg_path, x, y, scale=0.01):
-    """Plot an SVG at given coordinates on a map."""
-    with open(svg_path, 'r') as f:
-        svg = f.read()
-    path = parse_path(svg)
-    for idp, dp in enumerate(path.to_polygons()):
-        ax.plot(scale * dp[:, 0] + x, scale * dp[:, 1] + y, color="black")
-
-
 def query_by_feature_type(feature_type, all_data):
-    """
-    Query the dataset by feature type and return the corresponding GeoDataFrame.
-
-    Parameters:
-    - feature_type (str): The type of feature to query (e.g., 'airport', 'parking', etc.)
-
-    Returns:
-    - GeoDataFrame: A GeoDataFrame containing the queried features.
-    """
     feature_map = {
         'ports': 'ports-point',
         'parking': 'parking-polygon',
@@ -53,62 +32,37 @@ def query_by_feature_type(feature_type, all_data):
 data_folder = "data"
 all_data = load_shapefiles(data_folder)
 
-# Testing the query_by_feature_type function
-airports = query_by_feature_type('airport', all_data)
-print(airports.head())  # Print the first few rows of the airports data
+# Create a base map
+m = folium.Map(location=[45.75, 126.65], zoom_start=10)
 
-boundary_data = gpd.read_file(os.path.join(data_folder, 'boundary-polygon.shp'))
+# Define a color mapping
+color_map = {
+    "airport-polygon": "yellow",
+    "highway-line": "blue",
+    "railway-line": "red",
+    "water-polygon": "cyan",
+    "vegetation-polygon": "green",
+    "elevation-line": "brown",
+    "nature_reserve-polygon": "lime",
+    "land-polygon": "gray"
+}
 
-# Base Layer: Boundary of Harbin City
-ax = boundary_data.plot(figsize=(15, 15), color='lightgray', edgecolor='black')
+# Overlay features with specified colors
+for feature, color in color_map.items():
+    if feature in all_data:
+        folium.GeoJson(
+            all_data[feature],
+            name=feature.split('-')[0].capitalize(),
+            style_function=lambda feature, color=color: {
+                'fillColor': color,
+                'color': color,
+                'weight': 1.5,
+                'dashArray': '5, 5'
+            }
+        ).add_to(m)
 
-# Overlay Airports (if available in the dataset)
-if "airport-polygon" in all_data:
-    all_data["airport-polygon"].plot(ax=ax, color='yellow', label='Airports')
+# Add a layer control
+folium.LayerControl().add_to(m)
 
-# Overlay Highways (if available in the dataset)
-if "highway-line" in all_data:
-    all_data["highway-line"].plot(ax=ax, color='blue', label='Highways', linewidth=0.5)
-
-# Overlay Railways (if available in the dataset)
-if "railway-line" in all_data:
-    all_data["railway-line"].plot(ax=ax, color='red', label='Railways', linewidth=1)
-
-#TOPOGRAPHY
-
-# Overlay Water Bodies (if available in the dataset)
-if "water-polygon" in all_data:
-    all_data["water-polygon"].plot(ax=ax, color='cyan', label='Water Bodies')
-    
-# Overlay Vegetation
-if "vegetation-polygon" in all_data:
-    all_data["vegetation-polygon"].plot(ax=ax, color='green', label='Vegetation')
-
-# Overlay Elevation Contours
-if "elevation-line" in all_data:
-    all_data["elevation-line"].plot(ax=ax, color='brown', label='Elevation Contours', linewidth=0.5)
-
-# Overlay Nature Reserves
-if "nature_reserve-polygon" in all_data:
-    all_data["nature_reserve-polygon"].plot(ax=ax, color='lime', label='Nature Reserves', alpha=0.7)
-
-# Overlay Landuse
-if "land-polygon" in all_data:
-    all_data["land-polygon"].plot(ax=ax, cmap='tab20b', label='Land', alpha=0.5)
-    
-#Custom legend
-legend_elements = [
-    Line2D([0], [0], color='yellow', lw=2, label='Airports'),
-    Line2D([0], [0], color='blue', lw=2, label='Highways'),
-    Line2D([0], [0], color='red', lw=2, label='Railways'),
-    Line2D([0], [0], color='cyan', lw=2, label='Water Bodies'),
-    Line2D([0], [0], color='green', lw=2, label='Vegetation'),
-    Line2D([0], [0], color='brown', lw=2, label='Elevation Contours'),
-    Line2D([0], [0], color='lime', lw=2, label='Nature Reserves'),
-    Line2D([0], [0], marker='o', color='w', markerfacecolor='gray', markersize=10, label='Land')
-]
-
-# Set title and legend
-plt.title('Harbin City with physical Features')
-plt.legend(handles=legend_elements)
-plt.show()
+# Save the map to an HTML file
+m.save("map_output.html")
